ebcb4eb5dd5d152499f9dc1c74fe526d352450d4
nichia <nichia@gmail.com>
2020-02-02 13:31:22 -0500

    Add dropdown menu for searchTerm

--- src/components/SearchPetworxForm.js
+++ src/components/SearchPetworxForm.js
@@ -1,31 +1,76 @@
 import React, { useState } from 'react';
 import {Button, Container, Form} from 'reactstrap';
+import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 const SearchPetworxForm = (props) => {
  
   const [zipcode, setZipcode] = useState('');
 
+  const [searchTerm, setSearchTerm] = useState('dog parks');
+
+  const [dropdownOpen, setOpen] = useState(false);
+
+  const toggle = () => setOpen(!dropdownOpen);
+
   const handleSubmit = event => {
-    event.preventDefault()
-    props.fetchYelpApi(zipcode)
+    event.preventDefault();
+    props.fetchYelpApi(zipcode, searchTerm);
   }
+ 
+  const onDropdownClick = event => {
+    const newSearchTerm = event.currentTarget.getAttribute("dropdownvalue");
 
-  const onChange = event => {
-    setZipcode({
-      [event.target.name]: event.target.value
-    })
+    setSearchTerm(newSearchTerm);
+    props.fetchYelpApi(zipcode, newSearchTerm);
   }
-
+  
   return (
     <>
       <Container><h1>Find a pet service near you.</h1></Container>
       <Container>
         <Form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Search by zipcode" name="zipcode" onChange={onChange} required />        
-          <Button type="submit" value="Search">Search</Button>             
+        <ButtonDropdown isOpen={dropdownOpen} toggle={toggle}>
+            <DropdownToggle caret>
+              {searchTerm}
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem
+                onClick={onDropdownClick} dropDownValue="dog parks"
+              >dog parks</DropdownItem>
+              <DropdownItem
+                onClick={onDropdownClick} dropDownValue="pet stores"
+              >pet stores</DropdownItem>
+              <DropdownItem
+                onClick={onDropdownClick} dropDownValue="pet groomers"
+              >pet groomers</DropdownItem>
+              <DropdownItem
+                onClick={onDropdownClick} dropDownValue="pet services"
+              >pet services</DropdownItem>
+              <DropdownItem
+                onClick={onDropdownClick} dropDownValue="animal shelters"
+              >animal shelters</DropdownItem>
+              <DropdownItem
+                onClick={onDropdownClick} dropDownValue="animal hospital"
+              >animal hospital</DropdownItem>
+            </DropdownMenu>
+          </ButtonDropdown>
+
+          <input 
+            type="text" 
+            placeholder="Search by zipcode" 
+            name="zipcode" 
+            onChange={event => {
+              setZipcode(event.target.value); 
+            }} 
+            required 
+          />
+
+          <Button type="submit" value="Search">Search</Button>          
+
         </Form>
       </Container>
     </>
   )
 }
-export default SearchPetworxForm
+
+export default SearchPetworxForm;
--- src/containers/PetworxListContainer.js
+++ src/containers/PetworxListContainer.js
@@ -10,13 +10,13 @@ class PetworxListContainer extends Component {
   }
 
   componentDidMount() {
-    console.log("%ccomponentDidMount()", "color:purple;")
-    this.fetchYelpApi()
+    console.log("%ccomponentDidMount()", "color:purple;");
+    this.fetchYelpApi();
   }
 
-  fetchYelpApi = (zipcode) => {
-    const url = `https://api.yelp.com/v3/businesses/search?term=pet_services&location=${zipcode}&limit=20`
-    const proxyurl = "https://cors-anywhere.herokuapp.com/"
+  fetchYelpApi = (zipcode, searchTerm) => {
+    const url = `https://api.yelp.com/v3/businesses/search?term=${searchTerm}&location=${zipcode}&limit=20`;
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
     axios.get(proxyurl + url, {
       headers: {
         Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`
@@ -24,21 +24,21 @@ class PetworxListContainer extends Component {
     })
     .then((res) => {
       console.log("res:", res);
-      let petworx = []
+      let petworx = [];
       res.data.businesses.map((business) => (
         petworx.push(business)
-      ))
+      ));
       this.setState({
         petworxList: petworx
-      })
+      });
     })
     .catch((err) => {
-      console.log( 'error:', err )
+      console.log( 'error:', err );
     })
   }
 
   render() {
-    console.log("%crender()", "color:green;")
+    console.log("%crender()", "color:green;");
 
     return (
       <div>
@@ -52,4 +52,4 @@ class PetworxListContainer extends Component {
   }
 }
 
-export default PetworxListContainer
+export default PetworxListContainer;
